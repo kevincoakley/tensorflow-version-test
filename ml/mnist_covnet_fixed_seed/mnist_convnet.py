bb3e49611d42c256a207caec78f88da328af7147
Kevin Coakley <kcoakley@sdsc.edu>
2022-03-15 14:20:40 -0700

    Train the model 5 times

--- ml/mnist_covnet_fixed_seed/mnist_convnet.py
+++ ml/mnist_covnet_fixed_seed/mnist_convnet.py
@@ -2,13 +2,8 @@
 Title: Simple MNIST convnet
 Author: [fchollet](https://twitter.com/fchollet)
 Date created: 2015/06/19
-Last modified: 2020/04/21
+Last modified: 2022/03/15 (kevincoakley)
 Description: A simple convnet that achieves ~99% test accuracy on MNIST.
-"""
-
-"""
-2022/03/15 - kevincoakley
-Fix the random seed for reproducible results with Keras
 """
 
 """
@@ -21,71 +16,77 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 
-"""
-## Prepare the data
-"""
+def simple_mnist_covnet():
+    """
+    ## Prepare the data
+    """
 
-# Model / data parameters
-num_classes = 10
-input_shape = (28, 28, 1)
+    # Model / data parameters
+    num_classes = 10
+    input_shape = (28, 28, 1)
 
-# Fix the random seed for reproducible results with Keras
-seed_val = 1
-seed(seed_val)
-tf.random.set_seed(seed_val)
+    # Fix the random seed for reproducible results with Keras
+    seed_val = 1
+    seed(seed_val)
+    tf.random.set_seed(seed_val)
 
-# the data, split between train and test sets
-(x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+    # the data, split between train and test sets
+    (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
 
-# Scale images to the [0, 1] range
-x_train = x_train.astype("float32") / 255
-x_test = x_test.astype("float32") / 255
-# Make sure images have shape (28, 28, 1)
-x_train = np.expand_dims(x_train, -1)
-x_test = np.expand_dims(x_test, -1)
-print("x_train shape:", x_train.shape)
-print(x_train.shape[0], "train samples")
-print(x_test.shape[0], "test samples")
+    # Scale images to the [0, 1] range
+    x_train = x_train.astype("float32") / 255
+    x_test = x_test.astype("float32") / 255
+    # Make sure images have shape (28, 28, 1)
+    x_train = np.expand_dims(x_train, -1)
+    x_test = np.expand_dims(x_test, -1)
+    print("x_train shape:", x_train.shape)
+    print(x_train.shape[0], "train samples")
+    print(x_test.shape[0], "test samples")
 
 
-# convert class vectors to binary class matrices
-y_train = keras.utils.to_categorical(y_train, num_classes)
-y_test = keras.utils.to_categorical(y_test, num_classes)
+    # convert class vectors to binary class matrices
+    y_train = keras.utils.to_categorical(y_train, num_classes)
+    y_test = keras.utils.to_categorical(y_test, num_classes)
 
-"""
-## Build the model
-"""
+    """
+    ## Build the model
+    """
 
-model = keras.Sequential(
-    [
-        keras.Input(shape=input_shape),
-        layers.Conv2D(32, kernel_size=(3, 3), activation="relu"),
-        layers.MaxPooling2D(pool_size=(2, 2)),
-        layers.Conv2D(64, kernel_size=(3, 3), activation="relu"),
-        layers.MaxPooling2D(pool_size=(2, 2)),
-        layers.Flatten(),
-        layers.Dropout(0.5),
-        layers.Dense(num_classes, activation="softmax"),
-    ]
-)
+    model = keras.Sequential(
+        [
+            keras.Input(shape=input_shape),
+            layers.Conv2D(32, kernel_size=(3, 3), activation="relu"),
+            layers.MaxPooling2D(pool_size=(2, 2)),
+            layers.Conv2D(64, kernel_size=(3, 3), activation="relu"),
+            layers.MaxPooling2D(pool_size=(2, 2)),
+            layers.Flatten(),
+            layers.Dropout(0.5),
+            layers.Dense(num_classes, activation="softmax"),
+        ]
+    )
 
-model.summary()
+    model.summary()
 
-"""
-## Train the model
-"""
+    """
+    ## Train the model
+    """
 
-batch_size = 128
-epochs = 15
+    batch_size = 128
+    epochs = 15
 
-model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
+    model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
 
-model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, validation_split=0.1)
+    model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, validation_split=0.1)
 
-"""
-## Evaluate the trained model
-"""
+    """
+    ## Evaluate the trained model
+    """
 
-score = model.evaluate(x_test, y_test, verbose=0)
-print("Test loss:", score[0])
-print("Test accuracy:", score[1])
+    score = model.evaluate(x_test, y_test, verbose=0)
+    print("Test loss:", score[0])
+    print("Test accuracy:", score[1])
+
+if __name__ == '__main__':
+    for x in range(5):
+        print("MNIST Covnet Count: %s\n======================\n" % str(x + 1))
+        simple_mnist_covnet()
