55cf4e8ca9ccce2544b3a691e04c52d5278b351b
Kevin Coakley <kcoakley@sdsc.edu>
2022-03-28 15:04:34 -0700

    Generate files for Anaconda tests

---
+++ anaconda/environments/create_files.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+
+import yaml
+from jinja2 import Environment, FileSystemLoader
+
+# Read the variables for the tests
+with open("test_versions.yml") as f:
+    test_versions = yaml.safe_load(f)
+
+
+# Use a jinja template to create the Anaconda environment files
+env = Environment(loader=FileSystemLoader("templates"), trim_blocks=True,  lstrip_blocks=True)
+template = env.get_template("environment.yml.j2")
+
+for test_variables in test_versions:
+    parsed_template = template.render(test_variables)
+ 
+    with open("environment-tf%s-py%s-%s.yml" % (test_variables["tensorflow_version"],
+    test_variables["python_version"], test_variables["math_library"]), "w") as fh:
+        fh.write(parsed_template)
+
+
+# Use a jinja template to create the shell script to run the tests
+template = env.get_template("run_local_tests.sh.j2")
+parsed_template = template.render(test_versions=test_versions)
+
+with open("run_local_tests.sh", "w") as fh:
+    fh.write(parsed_template)
+
+
+# Use a jinja template to create the Singularity defintion files
+env = Environment(loader=FileSystemLoader("templates"))
+template = env.get_template("singularity.def.j2")
+
+for test_variables in test_versions:
+    parsed_template = template.render(test_variables)
+ 
+    with open("miniconda-tf%s-py%s-%s.def" % (test_variables["tensorflow_version"],
+    test_variables["python_version"], test_variables["math_library"]), "w") as fh:
+        fh.write(parsed_template)
+
+
+# Use a jinja template to create the shell script to run the tests
+template = env.get_template("build_singularity.sh.j2")
+parsed_template = template.render(test_versions=test_versions)
+
+with open("build_singularity.sh", "w") as fh:
+    fh.write(parsed_template)
+
+
+# Use a jinja template to create the shell script to run the tests
+template = env.get_template("run_singularity_tests.sh.j2")
+parsed_template = template.render(test_versions=test_versions)
+
+with open("run_singularity_tests.sh", "w") as fh:
+    fh.write(parsed_template)
