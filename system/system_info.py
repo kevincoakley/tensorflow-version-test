379a2a3e43e4e9532dd1b77ec760bce578150515
Kevin Coakley <kcoakley@sdsc.edu>
2022-05-03 10:18:19 -0700

    1.3.0 - system_info.py
    Save the system_info to a yaml file if ran via the command line

--- system/system_info.py
+++ system/system_info.py
@@ -7,9 +7,11 @@ import shutil
 import subprocess
 import sysconfig
 import xmltodict
+import yaml
 
 
-version = "1.2.0"
+version = "1.3.0"
+
 
 def get_cpu_info():
     proc_cpuinfo_file = "/proc/cpuinfo"
@@ -32,7 +34,7 @@ def get_cpu_info():
                     core_info = {}
 
                 # Add element to the dict
-                core_info[proc_cpuinfo_line_list[0]] = proc_cpuinfo_line_list[1]      
+                core_info[proc_cpuinfo_line_list[0]] = proc_cpuinfo_line_list[1]
 
         cpu_info.append(core_info)
         proc_cpuinfo_object.close()
@@ -40,7 +42,7 @@ def get_cpu_info():
         return cpu_info
     else:
         return None
-    
+
 
 def get_gpu_info():
     if shutil.which("nvidia-smi") != None:
@@ -49,8 +51,8 @@ def get_gpu_info():
             nvidia_smi_xml = subprocess.check_output(["nvidia-smi", "-q", "-x"])
         except subprocess.CalledProcessError as e:
             return None
-        
-        # Convert the XML output from nvidia-smi to a dict and return 
+
+        # Convert the XML output from nvidia-smi to a dict and return
         return xmltodict.parse(nvidia_smi_xml)
     else:
         return None
@@ -62,7 +64,7 @@ def get_os_info():
     if os.path.exists(os_release_file):
         os_info = {}
 
-        # Read os_release_file 
+        # Read os_release_file
         os_release_object = open(os_release_file, "r")
 
         for line in os_release_object.readlines():
@@ -70,7 +72,7 @@ def get_os_info():
             if line.strip() != "":
                 # Covert line to list on =
                 os_release_line_list = [x.strip() for x in line.split("=")]
-     
+
                 # Add element to the dict
                 os_info[os_release_line_list[0]] = os_release_line_list[1]
 
@@ -84,16 +86,20 @@ def get_os_info():
 def get_dmi_info():
     dmi_info = {}
 
-    dmiinfo_files = ["/sys/class/dmi/id/product_family",
-                     "/sys/class/dmi/id/product_name",
-                     "/sys/class/dmi/id/board_vendor"]
-    
+    dmiinfo_files = [
+        "/sys/class/dmi/id/product_family",
+        "/sys/class/dmi/id/product_name",
+        "/sys/class/dmi/id/board_vendor",
+    ]
+
     for dmiinfo_file in dmiinfo_files:
         if os.path.exists(dmiinfo_file):
             dmiinfo_object = open(dmiinfo_file, "r")
             os.path.basename(os.path.normpath(dmiinfo_file))
 
-            dmi_info[os.path.basename(os.path.normpath(dmiinfo_file))] = dmiinfo_object.readline().strip()
+            dmi_info[
+                os.path.basename(os.path.normpath(dmiinfo_file))
+            ] = dmiinfo_object.readline().strip()
             dmiinfo_object.close()
 
     if dmi_info == {}:
@@ -107,7 +113,7 @@ def get_environ_info():
 
     for key, value in os.environ.items():
         environ_info[key] = value
-    
+
     return environ_info
 
 
@@ -127,5 +133,8 @@ def get_system_info():
     return system_info
 
 
-if __name__ == '__main__':
-    pprint.pprint(get_system_info())
+if __name__ == "__main__":
+    sysinfo = get_system_info()
+
+    with open("system_info.yaml", "w") as system_info_file:
+        yaml.dump(sysinfo, system_info_file, default_flow_style=False)
